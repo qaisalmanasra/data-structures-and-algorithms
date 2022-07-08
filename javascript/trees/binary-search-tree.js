57f55b096025740d6070e609ac5b836c00d26f9b
Qais Almanasra <qmanasra@gmail.com>
2022-07-08 03:36:40 +0300

    all done

--- javascript/trees/binary-search-tree.js
+++ javascript/trees/binary-search-tree.js
@@ -57,6 +57,21 @@ class BinarySearchTree {
     traverse(this.root);
     return max;
   }
+  breadthFirst(tree) {
+    let front = [tree.root];
+    let result = [];
+    while (front.length) {
+      let current = front.shift();
+      result.push(current.value);
+      if (current.left) {
+        front.push(current.left);
+      }
+      if (current.right) {
+        front.push(current.right);
+      }
+    }
+    return result;
+  }
 }
 
 module.exports = BinarySearchTree;
